34b9c7653dbb55d4bde2c8b2223dab31a06f0a06
juan esteban <juan.garciam1@udea.edu.co>
2022-09-26 16:54:30 -0500

    se crea tabla en vista estudiante

--- src/components/Subject/Student/StudentView.jsx
+++ src/components/Subject/Student/StudentView.jsx
@@ -6,10 +6,9 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import GeneralInfo from "./GeneralInfo";
 import { SpecificInfo } from "./SpecificInfo";
 import ContentInfo from "./ContentInfo";
-
+import { TeacherInfo } from "./TeacherInfo";
 
 export const StudentView = ({ idSubjectSelected, readOnly }) => {
-
   return (
     <div>
       <Accordion>
@@ -21,7 +20,7 @@ export const StudentView = ({ idSubjectSelected, readOnly }) => {
           <Typography>Información General</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <GeneralInfo readOnly={readOnly}/>
+          <GeneralInfo readOnly={readOnly} />
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -33,7 +32,7 @@ export const StudentView = ({ idSubjectSelected, readOnly }) => {
           <Typography>Información Especifica</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <SpecificInfo readOnly={readOnly}/>
+          <SpecificInfo readOnly={readOnly} />
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -45,7 +44,20 @@ export const StudentView = ({ idSubjectSelected, readOnly }) => {
           <Typography>Contenido Resumido</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <ContentInfo readOnly={readOnly}/>
+          <ContentInfo readOnly={readOnly} />
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Coordinador de núcleo - Area academica</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <TeacherInfo />
         </AccordionDetails>
       </Accordion>
     </div>
